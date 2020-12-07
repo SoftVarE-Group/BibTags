ce54a9dad8a63268ae8e8f966fd20996d6ffc3b8
h3ssto <tobias.hess@uni-ulm.de>
2020-12-07 14:52:02 +0100

    Work on CI, added MYshort.bib to validation

--- .travis/validate.py
+++ .travis/validate.py
@@ -3,20 +3,56 @@
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 
-parser = BibTexParser(common_strings=True)
+def validate_with_abrv():
 
-#Required as Thomas uses @masterthesis
-parser.ignore_nonstandard_types = False 
+	print("> MYabrv.bib")
 
-bibtex_str = ""
-with open("MYabrv.bib") as bibtex_file:
-   bibtex_str = bibtex_file.read()
+	parser = BibTexParser(common_strings=True)
 
-with open("literature.bib") as bibtex_file:
-   bibtex_str += bibtex_file.read()
+	#Required as Thomas uses @masterthesis
+	parser.ignore_nonstandard_types = False 
 
-try:
-	bibtex_database = bibtexparser.loads(bibtex_str, parser)
-except bibtexparser.bibdatabase.UndefinedString as use:
-	print(f"Variable not found: {use}")
-	exit(1)
+	bibtex_str = ""
+	with open("MYabrv.bib") as bibtex_file:
+	   bibtex_str = bibtex_file.read()
+
+	with open("literature.bib") as bibtex_file:
+	   bibtex_str += bibtex_file.read()
+
+	try:
+		bibtex_database = bibtexparser.loads(bibtex_str, parser)
+	except bibtexparser.bibdatabase.UndefinedString as use:
+		print("[ERROR] MYabrv.bib")
+		print(f"Variable not found: {use}")
+		exit(1)
+
+	print("\nSUCCESS\n")
+
+def validate_with_short():
+
+	print("> MYshort.bib")
+
+	parser = BibTexParser(common_strings=True)
+
+	#Required as Thomas uses @masterthesis
+	parser.ignore_nonstandard_types = False 
+
+	bibtex_str = ""
+	with open("MYshort.bib") as bibtex_file:
+	   bibtex_str = bibtex_file.read()
+
+	with open("literature.bib") as bibtex_file:
+	   bibtex_str += bibtex_file.read()
+
+	try:
+		bibtex_database = bibtexparser.loads(bibtex_str, parser)
+	except bibtexparser.bibdatabase.UndefinedString as use:
+		print(f"Variable not found: {use}")
+		exit(1)
+
+	print("\nSUCCESS\n")
+
+
+validate_with_abrv()
+validate_with_short()
+
