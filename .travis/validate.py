4bac2d6ff8477be22a8215cb8ed4a4eded09cc23
h3ssto <tobias.hess@uni-ulm.de>
2020-12-07 14:04:17 +0100

    Added CI

---
+++ .travis/validate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+
+parser = BibTexParser(common_strings=True)
+
+#Required as Thomas uses @masterthesis
+parser.ignore_nonstandard_types = False 
+
+bibtex_str = ""
+with open("../MYabrv.bib") as bibtex_file:
+   bibtex_str = bibtex_file.read()
+
+with open("../literature.bib") as bibtex_file:
+   bibtex_str += bibtex_file.read()
+
+try:
+	bibtex_database = bibtexparser.loads(bibtex_str, parser)
+except bibtexparser.bibdatabase.UndefinedString as use:
+	print(f"Variable not found: {use}")
+	exit(1)
