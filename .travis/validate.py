d3c4c9545aa32854f27079beb85806daa539d488
h3ssto <tobias.hess@uni-ulm.de>
2020-12-12 20:26:11 +0100

    Refined validate

--- .travis/validate.py
+++ .travis/validate.py
@@ -3,9 +3,9 @@
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 
-def validate_with_abrv():
+def validate(abrv, literature):
 
-	print("> MYabrv.bib")
+	print(f"> {abrv}")
 
 	parser = BibTexParser(common_strings=True)
 
@@ -28,31 +28,7 @@ def validate_with_abrv():
 
 	print("\nSUCCESS\n")
 
-def validate_with_short():
 
-	print("> MYshort.bib")
-
-	parser = BibTexParser(common_strings=True)
-
-	#Required as Thomas uses @masterthesis
-	parser.ignore_nonstandard_types = False 
-
-	bibtex_str = ""
-	with open("MYshort.bib") as bibtex_file:
-	   bibtex_str = bibtex_file.read()
-
-	with open("literature.bib") as bibtex_file:
-	   bibtex_str += bibtex_file.read()
-
-	try:
-		bibtex_database = bibtexparser.loads(bibtex_str, parser)
-	except bibtexparser.bibdatabase.UndefinedString as use:
-		print(f"Variable not found: {use}")
-		exit(1)
-
-	print("\nSUCCESS\n")
-
-
-validate_with_abrv()
-validate_with_short()
+validate(abrv="MYabrv.bib", literature="literature.bib")
+validate(abrv="MYshort.bib", literature="literature.bib")
 
