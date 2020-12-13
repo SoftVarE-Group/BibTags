3dc7e4a05e94ca7ba6f6c92a2a1a968eef1ced46
h3ssto <tobias.hess@uni-ulm.de>
2020-12-13 12:29:16 +0100

    Fixed validate.py

--- .travis/validate.py
+++ .travis/validate.py
@@ -13,16 +13,16 @@ def validate(abrv, literature):
 	parser.ignore_nonstandard_types = False 
 
 	bibtex_str = ""
-	with open("MYabrv.bib") as bibtex_file:
+	with open(abrv) as bibtex_file:
 	   bibtex_str = bibtex_file.read()
 
-	with open("literature.bib") as bibtex_file:
+	with open(literature) as bibtex_file:
 	   bibtex_str += bibtex_file.read()
 
 	try:
 		bibtex_database = bibtexparser.loads(bibtex_str, parser)
 	except bibtexparser.bibdatabase.UndefinedString as use:
-		print("[ERROR] MYabrv.bib")
+		print(f"[ERROR] {abrv}")
 		print(f"Variable not found: {use}")
 		exit(1)
 
@@ -31,4 +31,3 @@ def validate(abrv, literature):
 
 validate(abrv="MYabrv.bib", literature="literature.bib")
 validate(abrv="MYshort.bib", literature="literature.bib")
-
