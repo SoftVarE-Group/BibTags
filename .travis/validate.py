783057da309794c425901537008594b83c91371e
Tobias Heß <58979585+h3ssto@users.noreply.github.com>
2020-12-21 15:41:31 +0100

    Merge CI (#9)
    Adds Travis CI to this repo.

---
+++ .travis/validate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+
+def validate(abrv, literature):
+
+	print(f"> {abrv}")
+
+	parser = BibTexParser(common_strings=True)
+
+	#Required as Thomas uses @masterthesis
+	parser.ignore_nonstandard_types = False 
+
+	bibtex_str = ""
+	with open(abrv) as bibtex_file:
+	   bibtex_str = bibtex_file.read()
+
+	with open(literature) as bibtex_file:
+	   bibtex_str += bibtex_file.read()
+
+	try:
+		bibtex_database = bibtexparser.loads(bibtex_str, parser)
+	except bibtexparser.bibdatabase.UndefinedString as use:
+		print(f"[ERROR] {abrv}")
+		print(f"Variable not found: {use}")
+		exit(1)
+
+	print("\nSUCCESS\n")
+
+
+validate(abrv="MYabrv.bib", literature="literature.bib")
+validate(abrv="MYshort.bib", literature="literature.bib")
