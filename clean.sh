884267d120d976b2c66df42ad3c42307f7627925
Sebastian Krieter <sebastian.krieter@ovgu.de>
2022-04-15 10:20:33 +0200

    Added copy function to clean script

--- clean.sh
+++ clean.sh
@@ -1,2 +1,8 @@
 java -jar mibtex/mibtex-cleaner.jar "literature.bib"
+if [ "$1" ]; then
+  cp literature-cleaned.bib $1
+  cp MYabrv.bib $1
+  cp MYshort.bib $1
+  cp MYfull.bib $1
+fi
 read -n1 -r -p "Press any key to continue..." key
