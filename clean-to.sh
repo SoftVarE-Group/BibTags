3f09044f312b6a949abd2dcb738ea4dcdb35a131
pmbittner <paul.bittner@uni-ulm.de>
2024-12-20 19:22:04 +0100

    clean-to.sh: more platform independent shebang

--- clean-to.sh
+++ clean-to.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 java -cp mibtex/mibtex-cleaner.jar de.mibtex.BibtexCleaner "literature.bib" -o "$1/literature-cleaned.bib" "${@:2}"
 cp MYabrv.bib $1
 cp MYshort.bib $1
